f9c96bc66a9e048acc5045e5f73afca9dfa4e1a1
KarinaEly <karina.elyacoubi21@gmail.com>
2021-03-27 18:45:32 +0100

    navbar

--- header.php
+++ header.php
@@ -6,8 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Site integrée sur la base d'une maquette réalisée par un graphiste.">
     <title>zomentum</title>
-    <link rel="shortcut icon" href="logo.png" type="image/png">
+    <link rel="shortcut icon" href="images/logo.png" type="image/png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link href="//db.onlinewebfonts.com/c/9475d6a0b79c164f9f605673714e75d9?family=Circular+Std+Medium" rel="stylesheet" type="text/css"/>
 </head>
+
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a  class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active links" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Integrations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Company</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link links" href="#">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a id="demo" class="nav-link " href="#">Get a demo</a>
+        </li>
+        <li class="nav-item">
+            <img src="images/login.png" alt="login">
+          <a class="nav-link links" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php include "header.php" ; ?>
 
+<h1>Titre</h1>
+
 <?php include "footer.php" ; ?>
 
