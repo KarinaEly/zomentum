7a2f3e9ebaae38c1f183461fce6d77a76cdeae21
KarinaEly <karina.elyacoubi21@gmail.com>
2021-03-28 19:26:06 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,3 +1,49 @@
+   
+<!--footer-->
+<div class="bg-footer">
+    <div class="conteneur">
+        <div class="links-footer">
+            <p>Product</p>
+            <a href="">Features</a>
+            <a href="">Pricing</a>
+            <a href="">Integrations</a>
+            <a href="">Product</a>
+        </div>
+        <div class="links-footer">
+            <p>Company</p>
+            <a href="">About us</a>
+            <a href="">Contact us</a>
+            <a href="">Submit a ticket</a>
+            <a href="">Privacy policy</a>
+            <a href="">Terms & conditions</a>
+        </div>
+        <div class="links-footer">
+            <p>Users</p>
+            <a href="">Login</a>
+            <a href="">Get a demo</a>
+            <a href="">Talk to us</a>
+            <a href="">Privacy policy</a>
+            <a href="">Terms & conditions</a>
+        </div>
+        <div class="links-footer">
+            <p>Guides</p>
+            <a href="">MSP</a>
+            <a href="">MSP Sales</a>
+        </div>
+        <div class="links-footer">
+            <p>Contact us</p>
+            <a href="">Address</a>
+        </div>
+        
+    </div>
+    <div class="conteneur" >
+        <div class="copyright">
+            <p>@2020 Pactora Inc. All rights reserved.</p>
+            <p>Follow us on social</p>
+        </div>
+    </div>
+</div>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -7,47 +7,50 @@
     <meta name="description" content="Site integrée sur la base d'une maquette réalisée par un graphiste.">
     <title>zomentum</title>
     <link rel="shortcut icon" href="images/logo.png" type="image/png">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link href="//db.onlinewebfonts.com/c/9475d6a0b79c164f9f605673714e75d9?family=Circular+Std+Medium" rel="stylesheet" type="text/css"/>
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid">
-    <a  class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active links test" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link links test" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link links test" href="#">Integrations</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link links test" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link links test" href="#">Company</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link links test" href="#">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a id="demo" class="nav-link test " href="#">Get a demo</a>
-        </li>
-        <li class="nav-item">
-            <img src="images/login.png" alt="login">
-          <a class="nav-link links test" href="#">Login</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<div class="main"> 
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active links test" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link links test" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link links test" href="#">Integrations</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link links test" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link links test" href="#">Company</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link links test" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a id="demo" class="nav-link test " href="#">Get a demo</a>
+                    </li>
+                    <li class="nav-item">
+                        <img src="images/login.png" alt="login">
+                        <a class="nav-link links test" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
