98aa52e7ed5ae69cd00f3ccf6165fe79198cbf17
acs <k.el-yacoubi@codeur.online>
2021-03-29 14:42:44 +0200

    card

--- index.php
+++ index.php
@@ -57,9 +57,61 @@
  <img src="images/homme-vue.png" alt="homme avec une longue vue" />
 </p>
 <p>
- Your channel program on steroids
- 
+ Your channel program on steroids. 
+ Tired of findinf MSP's that can helps you boost yoyr channel sales? 
+ You're in luck beacause they're all here.
+
 </p>
+
+
+<p class="flotte">
+ <img src="images/homme-plante.png" alt="homme debout" />
+</p>
+<p>
+ Become the vendor everyone wants to buy from.
+  Gain brand authority and visibility with the help of the 
+  largest IT service ecosystem . Find customers who are 
+  looking for your solution right now!
+
+</p>
+
+
+<p class="flotte">
+ <img src="images/homme-et-fusée.png" alt="homme et fusée" />
+</p>
+<p>
+ It's like having Your best Salesman on autopilot.
+ Share collaterals and documents that are automatically branded.
+ Leverage a rich partner network that sells your solution exactly 
+ the way your best salesman would.
+
+
+</p>
+
+
+<p class="flotte">
+ <img src="images/tableau.png" alt="graphique" />
+</p>
+<p>
+ Track your channel performance. Monitor the health and revenue
+ of your channel program and streamline your campaign to win more
+ customers within Zomentum.
+
+</p>
+
+
+<p class="flotte">
+ <img src="images/croix.png" alt="#" />
+</p>
+<p>
+ Get The Best version of Your sales playbook.
+ Gather insights about top channel sellers and find
+ out what pitch and collaterals ensure maximum profitability.
+ Win happy customers with an optimized sales playbook.
+
+
+</p>
+
 
 <?php include "footer.php" ; ?>
 
