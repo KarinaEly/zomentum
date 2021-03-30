6607fcc6d0a44f8f2db1f09ac1d2deccade828fd
acs <k.el-yacoubi@codeur.online>
2021-03-30 11:36:47 +0200

    ajout texte

--- index.php
+++ index.php
@@ -3,19 +3,22 @@
 
 <div class="banniere">
   <img class="ban-img" src="images/banniere.png" alt="banniere">
-  <div class="texte_centrer">Find the right partners to fuel your business growth </div>
+  <div class="texte_centrer">Find the right partners to fuel your business growth.
+  </div>
+   <br> Join a vibrant community of MSP's to forge long-lasting relationships with a partners
+    <br>that help you create excellent customer experiences.</br></b></div>
 </div>
 
 </div>
 
 
 <div class="triangles1">
-  <img src="images/triangles.png" alt="image trangles vert">
+  <img src="images/triangles-removebg.png" alt="image trangles vert">
   
 </div>
 
 <div class="triangles2">
-  <img src="images/triangles.png" alt="image trangles vert">
+  <img src="images/triangles-removebg.png" alt="image trangles vert">
   
 </div>
 
@@ -27,9 +30,12 @@
 
 <div class="bg-vert position-picto">
 
-<div><img src="images/rond1.png" class="picto" alt="pictogramme"></div>
-<div><img src="images/rond2.png" class="picto" alt="pictogramme loupe"></div>
-<div><img src="images/rond3.png" class="picto" alt="pictogramme statistique"></div>
+<div>
+  <img src="images/rond1-removebg.png" class="picto" alt="pictogramme"> <p> Be a part of the only All-ine-One<br> platform for IT services</p></div>
+<div>
+  <img src="images/rond2-removebg.png" class="picto" alt="pictogramme loupe"><p>Come closer to finding your<br> next customer</p></div>
+<div>
+<img src="images/rond3-removebg.png" class="picto" alt="pictogramme statistique"><p>Grow your business with a<br> single click</p></div>
 </div>
 
 
@@ -39,8 +45,8 @@
 </p>
 <p>
  Your channel program on steroids. 
- Tired of findinf MSP's that can helps you boost yoyr channel sales? 
- You're in luck beacause they're all here.
+ <br>Tired of findinf MSP's that can helps you<br> boost your channel sales? 
+ You're in luck beacause they're all here.</br></br>
 
 </p>
 
@@ -50,9 +56,9 @@
 </p>
 <p>
  Become the vendor everyone wants to buy from.
-  Gain brand authority and visibility with the help of the 
-  largest IT service ecosystem . Find customers who are 
-  looking for your solution right now!
+  <br>Gain brand authority and visibility with the help of the </br>
+  largest IT service ecosystem . Find customers who are
+  <br>looking for your solution right now!</br>
 
 </p>
 
@@ -62,9 +68,11 @@
 </p>
 <p>
  It's like having Your best Salesman on autopilot.
- Share collaterals and documents that are automatically branded.
- Leverage a rich partner network that sells your solution exactly 
- the way your best salesman would.
+ <br>Share collaterals and documents that are</br>
+  automatically branded.
+ Leverage a rich partner
+ <br>network that sells your solution exactly 
+ the way your best salesman would.</br>
 
 
 </p>
@@ -74,9 +82,12 @@
  <img src="images/tableau.png" alt="graphique" />
 </p>
 <p>
- Track your channel performance. Monitor the health and revenue
- of your channel program and streamline your campaign to win more
- customers within Zomentum.
+ Track your channel performance.
+ <br> Monitor the health and revenue
+ of your channel</br>
+  program 
+  and streamline your campaign to win more
+ <br>customers within Zomentum.</br>
 
 </p>
 
@@ -86,9 +97,9 @@
 </p>
 <p>
  Get The Best version of Your sales playbook.
- Gather insights about top channel sellers and find
- out what pitch and collaterals ensure maximum profitability.
- Win happy customers with an optimized sales playbook.
+<br>Gather insights about top channel sellers and find</br>
+ out what pitch and collaterals ensure maximum
+<br>profitability. Win happy customers with an optimized sales playbook.</br>
 
 
 </p>
