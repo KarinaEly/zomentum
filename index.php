8876326be5550e1c08ee60a0a39ee8220b9268c1
acs <k.el-yacoubi@codeur.online>
2021-04-03 19:38:48 +0200

    update

--- index.php
+++ index.php
@@ -1,141 +1,115 @@
 <?php include "header.php" ; ?>
 
 <!--Bannière-->
-
 <div class="banniere">
- 
   <div class="texte-centrer">
     <div class="business">
       <h2>Find the right partners to<br> fuel your business growth</h2>
     </div>
-    
     <div class="partners">
       <p>Join a vibrant community of MSP's to forge long-lasting relationships with a partners<br>
     that help you create excellent customer experiences.</p>
-    </div>
     <input id="button"type="button"value="Register today"> 
-  </div>
-   
+    </div>
+  </div> 
 </div> 
 
 <!--backgroung color picto-->
-
 <div class="bg-vert "> 
-<div class="container">
-  <!--triangles vert droite-->  
-</div>
-<div class="triangles1">
-    <img src="images/triangles-removebg.png" alt="image trangles vert">
-  
-</div> 
-
-  <!--texte centrer-->
-
-<div class="centrer">
-
-  
-  <p>List.Sell.Grow</p>
-
-</div>
-
-  <!--pictogrammes rond-->
-<div class="position-picto col-12">
-  <div class="margin-picto">
-    <img src="images/rond1-removebg.png" class="picto" alt="pictogramme"> <p> Be a part of the only All-ine-One<br> platform for IT services</p>
-  </div>
-  <div class="margin-picto">
-    <img src="images/rond2-removebg.png" class="picto" alt="pictogramme loupe"><p>Come closer to finding your<br> next customer</p>
-  </div>
-  <div class="margin-picto" >
-      <img src="images/rond3-removebg.png" class="picto" alt="pictogramme statistique"><p>Grow your business with a<br> single click</p>
+  <div class="container">
+    <!--triangles vert droite-->  
+    <div class="triangles1">
+        <img src="images/triangles-removebg.png" alt="image trangles vert">
+    </div> 
+      <!--texte centrer-->
+    <div class="centrer">
+      <p>List.Sell.Grow</p>
+    </div>
+      <!--pictogrammes rond-->
+    <div class="position-picto col-12">
+      <div class="margin-picto">
+        <img src="images/rond1-removebg.png" class="picto" alt="pictogramme"> <p> Be a part of the only All-ine-One<br> platform for IT services</p>
+      </div>
+      <div class="margin-picto">
+        <img src="images/rond2-removebg.png" class="picto" alt="pictogramme loupe"><p>Come closer to finding your<br> next customer</p>
+      </div>
+      <div>
+          <img src="images/rond3-removebg.png" class="picto" alt="pictogramme statistique"><p>Grow your business with a<br> single click</p>
+      </div>
     </div>
     <div class="triangles2">
-      <img src="images/triangles-removebg.png" alt="image trangles vert">
+        <img src="images/triangles-removebg.png" alt="image trangles vert">
+    </div>
+  </div>
 </div>
+
+<div class="container">
+  <!--1ère illustartion-->          
+  <div class="illustration-text position-illustration-reverse margin-illustrations">
+    <div> 
+      <img class="illustration " src="images/homme-vue.png" alt="homme avec une longue vue"/>
+    </div>
+    <div> 
+      <h5 class="channel">Your channel program<br> on steroids </h5> 
+      <p class="sales padding-illustration">Tired of finding MSP's that can helps you boost your<br> channel sales? 
+        You're in luck beacause they're all here.</p>
+    </div>
   </div>
 
+  <!--2ème illustration-->
+
+  <div class="margin-illustrations position-illustration-reverse">
+    <div class="flotte-text">
+      <h5 class="channel">Become the vendor<br> everyone wants to buy from</h5>
+      <p class="sales padding-illustration">Gain brand authority and visibility with the help of the<br>
+      largest IT service ecosystem . Find customers who are<br>
+      looking for your solution right now.</p>
+    </div>
+    <div>
+      <img class="illustration" src="images/homme-plante.png" alt="homme debout"/>
+    </div>
   </div>
 
+  <!--3ème illustration-->
 
-</div>
-</div> 
-
-<!--1ère illustartion-->          
-
-
-<div class="centrer">
-
-<div class="flotte"> 
- <img class="illustration" src="images/homme-vue.png" alt="homme avec une longue vue"/>
-<div class="flotte-text">
-    <h5 class="channel">Your channel program<br> on steroids 
-</h5>
-  <p class="sales">Tired of finding MSP's that can helps you boost your<br> channel sales? 
-    You're in luck beacause they're all here.
-</p>
-</div>
-</div>
-
-<!--2ème illustration-->
-
-<div class="flotte">
-
-  <div class="flotte-text">
-    <h5 class="channel">Become the vendor<br> everyone wants to buy from</h5>
-    <p class="sales">Gain brand authority and visibility with the help of the<br>
-    largest IT service ecosystem . Find customers who are<br>
-    looking for your solution right now.</p>
+  <div class="margin-illustrations position-illustration-reverse"> 
+    <div>
+      <img class="illustration" src="images/homme-et-fusee-2.png" alt="homme et fusée"/>
+    </div>
+    <div class="flotte-text">
+      <h5 class="channel">It's like having Your best<br> Salesman on autopilot</h5>
+      <p class="sales padding-illustration">Share collaterals and documents that are<br>automatically branded.
+      Leverage a rich partner<br> network that sells your solution exactly the way<br> your best salesman would.
+      </p>
+    </div>
   </div>
 
-  <img class="illustration" src="images/homme-plante.png" alt="homme debout"/>
+  <!--4ème illustration-->
 
-</div>
-
-<!--3ème illustration-->
-
-<div class="flotte"> 
- <img class="illustration" src="images/homme-et-fusee-2.png" alt="homme et fusée"/>
-<div class="flotte-text">
-    <h5 class="channel">It's like having Your best<br> Salesman on autopilot
-</h5>
-  <p class="sales">Share collaterals and documents that are<br>automatically branded.
-    Leverage a rich partner<br> network that sells your solution exactly the way<br> your best salesman would.
-</p>
-</div>
-</div>
-
-<!--4ème illustration-->
-
-<div class="flotte">
-
-  <div class="flotte-text">
-    <h5 class="channel">Track your channel<br> performance</h5>
-    <p class="sales">Monitor the health and revenue of your channel<br> program and streamline your campaign to win more<br>
-    customers within Zomentum.</p>
+  <div class="margin-illustrations position-illustration-reverse">
+    <div class="flotte-text">
+      <h5 class="channel">Track your channel<br> performance</h5>
+      <p class="sales padding-illustration">Monitor the health and revenue of your channel<br> program and streamline your campaign to win more<br>
+      customers within Zomentum.</p>
+    </div>
+    <div>
+      <img class="illustration" src="images/tableau-3.png" alt="tableau"/>
+    </div>
   </div>
 
-  <img class="illustration" src="images/tableau-3.png" alt="tableau"/>
+  <!--5ème illustration-->
 
+  <div class="margin-illustrations position-illustration-reverse">
+    <div> 
+      <img class="illustration" src="images/croix-2.png" alt="homme avec une longue vue"/>
+    </div>
+    <div class="flotte-text">
+      <h5 class="channel">Get The Best version of<br> Your sales playbook</h5>
+      <p class="sales padding-illustration">Gather insights about top channel sellers and find<br>
+      out what pitch and collaterals ensure maximum<br> profitability. Win happy customers with an<br> optimized sales playbook.</p>
+    </div>
+  </div>
 </div>
-
-
-
-<!--5ème illustration-->
-
-
-<div class="centrer">
-
-<div class="flotte"> 
- <img class="illustration" src="images/croix-2.png" alt="homme avec une longue vue"/>
-<div class="flotte-text">
-    <h5 class="channel">Get The Best version of<br> Your sales playbook
-</h5>
-  <p class="sales">Gather insights about top channel sellers and find<br>
-    out what pitch and collaterals ensure maximum<br> profitability. Win happy customers with an<br> optimized sales playbook.
-</p>
-</div>
-</div>
-
 
 <?php include "footer.php" ; ?>
 
